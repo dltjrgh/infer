6a5c8ceca483de82dafb92071a345a93bbbdff7c
dltjrgh <andrewleebcs@gmail.com>
2024-12-30 17:54:32 +0000

    pretty print all sourcefiles

--- infer/src/symfuzz/SymbolicFuzzer.ml
+++ infer/src/symfuzz/SymbolicFuzzer.ml
@@ -1,7 +1,14 @@
 open! IStd
-(* module F = Format *)
+module F = Format
 (* module L = Logging *)
 
 let checker () =
-  let _ = print_endline "Hello World" in
+  let _ = print_endline "Before Logging" in
+  let _ =
+    SourceFiles.pp_all
+      ~filter:(fun _ -> true)
+      ~type_environment:true ~procedure_names:true ~freshly_captured:true (F.get_std_formatter ())
+      ()
+  in
+  let _ = print_endline "After Logging" in
   ()

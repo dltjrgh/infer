521b6c4edbded63a683c7495cac04876c7f64c83
dltjrgh <andrewleebcs@gmail.com>
2024-12-31 07:35:38 +0000

    access nodes from cfg

--- infer/src/symfuzz/SymbolicFuzzer.ml
+++ infer/src/symfuzz/SymbolicFuzzer.ml
@@ -3,12 +3,26 @@ module F = Format
 (* module L = Logging *)
 
 let checker () =
-  let _ = print_endline "Before Logging" in
+  let _ = print_endline "Start Logging SourceFiles.." in
   let _ =
     SourceFiles.pp_all
       ~filter:(fun _ -> true)
       ~type_environment:true ~procedure_names:true ~freshly_captured:true (F.get_std_formatter ())
       ()
   in
-  let _ = print_endline "After Logging" in
+  let _ = print_endline "Finish Logging SourceFiles.." in
+  let _ = print_endline "Start Logging Nodes.." in
+  let source_files = SourceFiles.get_all ~filter:(fun _ -> true) () in
+  let procnames =
+    List.concat_map source_files ~f:(fun src -> SourceFiles.proc_names_of_source src)
+  in
+  let _ =
+    List.map procnames ~f:(fun procname ->
+        match Procdesc.load procname with
+        | Some pdesc ->
+            Procdesc.pp_with_instrs (F.get_std_formatter ()) pdesc
+        | None ->
+            () )
+  in
+  let _ = print_endline "Finish Logging Nodes.." in
   ()
